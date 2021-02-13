6fd564ae764133f3b764f9f0cb6efb40568bac41
mishotek <mishotektumanidze@gmail.com>
2021-02-13 18:26:24 +0400

    # Build config updated

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { RouterFactory } from './router/router-factory';
+
+export const Router = RouterFactory.router;
+export { RmRouter } from './components/rm-router';
+export { RmRoute } from './components/rm-route';
